d0789caa364ad07ec1fada02c6992de627dd496e
saanpritom <pritom.saan@gmail.com>
2019-04-19 05:17:04 +0600

    Tag abbreviation working

--- textprocessor/textprocess.py
+++ textprocessor/textprocess.py
@@ -1,11 +1,71 @@
 from textblob import TextBlob
+from textprocessor.choices import *
+
+
+class ResultCounter:
+
+    def tag_abreviation_checker(self, abbr):
+
+        detail_data = 'Nothing'
+
+        for key, value in tag_details.items():
+            if key == abbr:
+                detail_data = value
+                break
+        return detail_data
+
+
+class ResultCleaner:
+
+    def first_bracket_first_parenthesis_remover(self, text):
+        return text.replace("(", "")
+
+    def first_bracket_second_parenthesis_remover(self, text):
+        return text.replace(")", "")
+
+    def single_quote_remover(self, text):
+        return text.replace("'", "")
+
+    def space_remover(self, text):
+        return text.replace(" ", "")
+
+    def tag_comma_splitter(self, text, index_value):
+        return text.split(',')[index_value]
 
 
 class TextProcess:
 
-    def text_tag(text):
+    result_counter = ResultCounter()
+    text_cleaner = ResultCleaner()
+    return_result = []
+
+    def text_tag(self, text):
         blob = TextBlob(text)
-        return blob.tags
+        blob = blob.tags
+
+        for data in blob:
+
+            ##remove the first bracket first parenthesis
+            data = self.text_cleaner.first_bracket_first_parenthesis_remover(str(data))
+
+            ##remove the first bracket second parenthesis
+            data = self.text_cleaner.first_bracket_second_parenthesis_remover(str(data))
+
+            ##remove the single quote from string
+            data = self.text_cleaner.single_quote_remover(str(data))
+
+            ##spliting comma from the string
+            first_value = self.text_cleaner.tag_comma_splitter(str(data), 0)
+            last_value = self.text_cleaner.tag_comma_splitter(str(data), 1)
+
+            ##detect abreviation
+            last_value = self.result_counter.tag_abreviation_checker(self.text_cleaner.space_remover(last_value))
+
+            data = first_value + ' ' + last_value
+
+            self.return_result.append(data)
+
+        return self.return_result
 
     def text_noun(text):
         blob = TextBlob(text)
--- textprocessor/views.py
+++ textprocessor/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.views.generic import View
 from textprocessor.forms import TextProcessingForm
 from textprocessor.textprocess import TextProcess
-from textprocessor.choices import *
 
 # Create your views here.
 class HomePageView(View):
@@ -10,7 +9,7 @@ class HomePageView(View):
     form_class = TextProcessingForm
     context_object_name = 'text_form'
     initial = {'key': 'value'}
-    processor_class = TextProcess
+    processor_class = TextProcess()
     form_error = False
 
     def get(self, request, *args, **kwargs):
@@ -31,22 +30,18 @@ class HomePageView(View):
                 if feature == 'Tags':
                     choices.append('Tags')
                     results['Tags'] = self.processor_class.text_tag(text)
-                    #results.append(self.processor_class.text_tag(text))
                 elif feature == 'Nouns':
                     choices.append('Nouns')
                     results['Nouns'] = self.processor_class.text_noun(text)
-                    #results.append(self.processor_class.text_noun(text))
                 elif feature == 'Words':
                     choices.append('Words')
                     results['Words'] = self.processor_class.text_word(text)
-                    #results.append(self.processor_class.text_word(text))
                 else:
                     choices.append('Sentences')
                     results['Sentences'] = self.processor_class.text_sentence(text)
-                    #results.append(self.processor_class.text_sentence(text))
 
 
-            return render(request, 'result-view.html', {'results': results, 'choices': choices, 'box_color': box_options, 'error': self.form_error})
+            return render(request, 'result-view.html', {'results': results, 'choices': choices, 'error': self.form_error})
 
         else:
             self.form_error = True
