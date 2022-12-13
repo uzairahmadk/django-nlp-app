2915a76adc4292b84f11e465d87b0aaea680078f
shijisohail <shijisohail786@gmail.com>
2022-12-13 11:43:32 +0500

    Search Fix

--- textprocessor/admin.py
+++ textprocessor/admin.py
@@ -3,12 +3,11 @@ from django.contrib import admin
 # Register your models here
 from .models import  *
 
-@admin.register(Project)
-class Search(admin.ModelAdmin):
-    list_display = ['name', 'technology','supervisor','created']
-    list_filter = ('name', 'technology','supervisor','created')
-    search_fields = ['name', 'technology','supervisor','created']
-
 admin.site.register(Department)
 admin.site.register(Supervision)
 admin.site.register(Technology)
+@admin.register(Project)
+class Project(admin.ModelAdmin):
+    list_filter = ('name', 'technology','supervisor','created')
+    list_display = ['name', 'technology','supervisor','created']
+    search_fields = ['name', 'technology__tech','supervisor__supervisor','created',]
