69e8ba6c41b12f2ba7c8074d6a628f9def3c609c
shijisohail <shijisohail786@gmail.com>
2022-12-27 16:46:24 +0500

    check

--- textprocessor/admin.py
+++ textprocessor/admin.py
@@ -1,13 +1,61 @@
 from django.contrib import admin
-
+from django.forms import forms
+from django.urls import path
+from django.http import HttpResponse
+from django.shortcuts import redirect,render
+import csv
 # Register your models here
 from .models import  *
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
 
 admin.site.register(Department)
 admin.site.register(Supervision)
 admin.site.register(Technology)
+class CsvImportForm(forms.Form):
+    csv_file = forms.FileField()
 @admin.register(Project)
-class Project(admin.ModelAdmin):
+class Project(admin.ModelAdmin,ExportCsvMixin):
     list_filter = ('name', 'technology','supervisor','created')
     list_display = ['name', 'technology','supervisor','created']
-    search_fields = ['name', 'technology__tech','supervisor__supervisor','created',]
+    search_fields = ['name', 'technology__tech','supervisor__supervisor','created']
+
+    change_list_template = "change_list.html"
+    try:
+        def get_urls(self):
+            urls = super().get_urls()
+            my_urls = [
+                path('import-csv/', self.import_csv),
+            ]
+            return my_urls + urls
+
+        def import_csv(self, request):
+            if request.method == "POST":
+                csv_file = request.FILES["csv_file"]
+                reader = csv.reader(csv_file)
+                # Create Hero objects from passed in data
+                # ...
+                self.message_user(request, "Your csv file has been imported")   
+                return redirect("..")
+            form = CsvImportForm()
+            payload = {"form": form}
+            return render(
+                request, "csv_form.html", payload
+            )
+    except Exception as ex:
+        print("New Function Exception: ",ex)
