7fb805ec6043dc212d6908ebab2373a011a4dab8
shijisohail <shijisohail786@gmail.com>
2022-12-29 22:09:27 +0500

    pychaches and announcements

--- textprocessor/admin.py
+++ textprocessor/admin.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from django.forms import forms
 from django.urls import path
 from django.http import HttpResponse
 from django.shortcuts import redirect,render
 import csv
 import pandas as pd
+
 # Register your models here
 from .models import  *
+from textprocessor.models import Project as ProjectModel
+
+admin.site.register(Department)
+admin.site.register(Supervision)
+admin.site.register(Technology)
+admin.site.register(Announcement)
+admin.site.register(User)
+class CsvImportForm(forms.Form):
+    csv_file = forms.FileField()
+
 class ExportCsvMixin:
     def export_as_csv(self, request, queryset):
 
@@ -25,13 +37,8 @@ class ExportCsvMixin:
 
     export_as_csv.short_description = "Export Selected"
 
-admin.site.register(Department)
-admin.site.register(Supervision)
-admin.site.register(Technology)
-class CsvImportForm(forms.Form):
-    csv_file = forms.FileField()
 @admin.register(Project)
-class Project(admin.ModelAdmin,ExportCsvMixin):
+class Project(admin.ModelAdmin):
     list_filter = ('name', 'technology','supervisor','created')
     list_display = ['name', 'technology','supervisor','created']
     search_fields = ['name', 'technology__tech','supervisor__supervisor','created']
@@ -49,8 +56,8 @@ class Project(admin.ModelAdmin,ExportCsvMixin):
                 csv_file = request.FILES["csv_file"] 
                 reader = pd.read_csv(csv_file)
                 reader = reader.fillna("")
-                for rows in reader.values.tolist:
-                    proj = Project.objects.create(                            
+                for rows in reader.values.tolist():
+                    proj = Project.objects.all(                            
                         name = rows[0] if rows[0] else None,
                         department =  rows[1] if rows[1] else None,
                         supervision =  rows[2] if rows[2] else None,
@@ -66,5 +73,3 @@ class Project(admin.ModelAdmin,ExportCsvMixin):
             )
     except Exception as ex:
         print("New Function Exception: ",ex)
-
-admin.site.register(Announcement)
